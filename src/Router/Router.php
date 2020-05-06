e25a3e49ef4246b7daed404767decccfa69519cb
Michael Birch <mbirch321@gmail.com>
2020-05-06 18:32:00 +1200

    Allow numerals in variables in url

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -34,7 +34,7 @@ class Router
         $route = preg_replace('/\//', '\\/', $route);
 
         // Convert variables e.g. {controller}
-        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[-0-9a-z]+)', $route);
 
         // Convert variables with custom regular expressions e.g. {id:\d+}
         $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
