56ef3e8f3303f3339bbd13269c4ef622d4b559d5
Michael Birch <mbirch321@gmail.com>
2020-05-12 21:30:06 +1200

    Add phpstan config

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+define("ROOT", dirname(__DIR__, 1));
+define("CONFIG", ROOT . DS . 'config');
+define("CACHE", ROOT . DS . 'cache');
+define("LOGS", ROOT . DS . 'logs');
+define("WEBROOT", ROOT . DS . 'public');
+define("APP", ROOT . DS . 'src');
+
+require ROOT. DS . 'vendor' . DS . 'autoload.php';
+
+set_error_handler('Showus\Error\Error::errorHandler');
+set_exception_handler('Showus\Error\Error::exceptionHandler');
