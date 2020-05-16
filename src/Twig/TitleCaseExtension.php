bd067af11447a6e6f6cc0afe51b32891bf625b1b
Michael Birch <mbirch321@gmail.com>
2020-05-16 20:32:46 +1200

    Add title case twig extension

---
+++ src/Twig/TitleCaseExtension.php
@@ -0,0 +1,26 @@
+<?php
+namespace Anticus\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+/**
+ * Convert a string to title case. 
+ * Useful for tags which are alphanumeric.
+ * Preserves uppercase tags like CSS or HTML, which are 
+ * converted to Css and Html by the Twig title filter.
+ */
+class TitleCaseExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return array(
+            new TwigFilter('title', [$this, 'titleCase'])
+        );
+    }
+
+    public function titleCase($title)
+    {
+        return ucwords($title);
+    }
+}
