b344e42045a172a8c925517c9c2537f64ec761a3
Michael Birch <mbirch321@gmail.com>
2020-05-17 21:50:13 +1200

    method for testing PDO

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -48,4 +48,41 @@ abstract class Model
         }
         return $pdo;
     }
+
+    /**
+     * Get an instance of PDO (or null) without throwing an error
+     *
+     * @return object|null
+     */
+    protected static function testPDO()
+    {
+        static $pdo = null;
+        $config = Configure::read();
+        $db_config = $config['Datasources'][$config['environment']];
+        if ($pdo === null) {
+            $dsn = 'mysql:host=' . 
+            $db_config['host'] . 
+            ';dbname=' . 
+            $db_config['name'] . 
+            ';charset=' . 
+            $db_config['charset'];
+            $options = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+   
+            try {
+                $pdo = new PDO(
+                    $dsn,
+                    $db_config['username'],
+                    $db_config['password'],
+                    $options
+                );
+            } catch (\PDOException $e) {
+                return $pdo;
+            }
+        }
+        return $pdo;
+    }
 }
