ec100afaf58e76bc5debaad215b7f740b2250969
Michael Birch <mbirch321@gmail.com>
2021-02-16 20:42:22 +1300

    Add twig function for CSRF protection

--- src/View/View.php
+++ src/View/View.php
@@ -55,6 +55,25 @@ class View
                     'cache' => $data['config']['paths']['Cache'] . DS . 'twig'
                 ]);
             }
+            // CSRF protection, https://stackoverflow.com/a/31683058/354196
+            $twig->addFunction(
+                new \Twig\TwigFunction(
+                    'form_token',
+                    function($lock_to = null) {
+                        if (empty($_SESSION['token'])) {
+                            $_SESSION['token'] = bin2hex(random_bytes(32));
+                        }
+                        if (empty($_SESSION['token2'])) {
+                            $_SESSION['token2'] = random_bytes(32);
+                        }
+                        if (empty($lock_to)) {
+                            return $_SESSION['token'];
+                        }
+                        
+                        return hash_hmac('sha256', $lock_to, $_SESSION['token2']);
+                    }
+                )
+            );
             $twig->addExtension(new HtmlExtension());
             $twig->addExtension(new ReadingTimeExtension());
             $twig->addExtension(new TitleCaseExtension());
