794315800f9482b6141c6264cd32feef37b63497
Michael Birch <mbirch321@gmail.com>
2021-07-10 15:32:38 +1200

    Show the url for 404 error

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -123,7 +123,7 @@ class Router
                 throw new \Exception("Controller class $controller not found");
             }
         } else {
-            throw new \Exception('No route matched.', 404);
+            throw new \Exception('No route matched for ' . $url, 404);
         }
     }
 
